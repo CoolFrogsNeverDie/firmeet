fa3c83313790ea556a7daa55322d1c923e5e12ac
min0808 <min0808mini@gmail.com>
2023-07-31 15:52:35 +0900

    안지은 완

---
+++ src/main/java/com/firmeet/dao/PayDAO.java
@@ -0,0 +1,32 @@
+package com.firmeet.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.firmeet.vo.NoticeBoardVO;
+import com.firmeet.vo.PayresultVO;
+
+@Repository
+public class PayDAO {
+	
+	@Autowired
+	SqlSession sql;
+	
+	public int payinsert(PayresultVO vo) {
+		System.out.println("payinsertDAO까지 오는지 확인" + vo);
+		sql.insert("pay.payinsert", vo);
+		return 0;
+	}
+	
+	public int payupdate(PayresultVO vo) {
+		System.out.println("payupdateDAO까지 오는지 확인" + vo);
+		return sql.update("pay.payupdate", vo);
+	}
+
+	public PayresultVO paycnt(NoticeBoardVO vo) {
+		System.out.println("DAO로 넘어오는 cnt 가져오기용"  + vo);
+		return sql.selectOne("pay.paycnt",vo);
+	}
+	
+}
